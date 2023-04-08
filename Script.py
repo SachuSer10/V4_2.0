f1c04910473e724d07e73aaf8ef3120b13724532
LOOK ME FROM EARTH <102791126+DHANANJAY4532@users.noreply.github.com>
2023-04-08 11:42:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 class script(object):   
     HELP_TXT = """𝙷𝙴𝚈 {}\n𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
-    HOO_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
 ✯ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 𝙼𝚔𝚗 𝙱𝚘𝚝𝚣"""
 
     SOURCE_TXT = """<b>NOTE:</b>
